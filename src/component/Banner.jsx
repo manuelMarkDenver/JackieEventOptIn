80e580d4be5ae49c3eb48badad0472e330b152c4
Mark Denver Manuel <manuel.markdenver@gmail.com>
2022-09-23 23:35:09 +0800

    initial commit

---
+++ src/component/Banner.jsx
@@ -0,0 +1,11 @@
+import { Typography } from "@mui/material";
+
+const Banner = () => {
+  return (
+    <Typography variant="h1" component="h2" align="center">
+      Jackie's Event
+    </Typography>
+  );
+};
+
+export default Banner;
